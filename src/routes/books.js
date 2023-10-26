eb574889e6b970f4392520a86b09fb412060b83a
hy-reza <hy.rezaalfanda@gmail.com>
2023-10-26 22:23:56 +0700

    apply image upload

--- src/routes/books.js
+++ src/routes/books.js
@@ -1,12 +1,19 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const {retriveBooks, addBook, retriveBook, removeBooks, updateBooks} = require("../controllers/bookControllers")
+const {
+  retriveBooks,
+  addBook,
+  retriveBook,
+  removeBooks,
+  updateBooks,
+} = require("../controllers/bookControllers");
+const { upload } = require("../middleware/multerMiddleware");
+const useFirebase = require("../middleware/firebaseMiddleware");
 
-
-router.get('/', retriveBooks);
-router.get('/:id', retriveBook)
-router.post('/', addBook);
-router.put('/:id', updateBooks)
-router.delete('/:id', removeBooks)
+router.get("/", retriveBooks);
+router.get("/:id", retriveBook);
+router.post("/", upload, useFirebase, addBook);
+router.put("/:id", upload, useFirebase, updateBooks);
+router.delete("/:id", removeBooks);
 
 module.exports = router;
