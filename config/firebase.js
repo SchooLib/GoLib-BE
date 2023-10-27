69b615f1baec24b3f18cf7c863f3a01a426e5295
hy-reza <hy.rezaalfanda@gmail.com>
2023-10-27 09:12:17 +0700

    remove coments

--- config/firebase.js
+++ config/firebase.js
@@ -10,11 +10,7 @@ const {
   messagingSenderId,
   appId,
 } = require("./config");
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: apiKey,
   authDomain: authDomain,
